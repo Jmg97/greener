78ecab66adf0c9507040b24e123bb17215e6e8a2
Jmg97 <92910931+Jmg97@users.noreply.github.com>
2022-01-04 16:48:36 +0900

    slide banner tq

--- btn.js
+++ btn.js
@@ -1,20 +1,35 @@
-let btnOne = document.querySelector(".btn-one");
-let btnTwo = document.querySelector(".btn-two");
-let btnThree = document.querySelector(".btn-three");
-const banner = document.querySelector(".banner-box");
+//  슬라이드 효과 // 닷 버튼 클릭 시 모양 변경
+
+
+let btnOne = document.querySelector(".btn1");
+let btnTwo = document.querySelector(".btn2");
+let btnThree = document.querySelector(".btn3");
+const banner = document.querySelector(".image-main-box");
+const dot = document.querySelector(".dot")
 
 function oneSlide(){
-    banner.style.transform = "translate(0vw)"
-}
+    banner.style.transform = "translateX(0%)";
+     btnOne.classList.add("btn-click");
+     btnTwo.classList.remove("btn-click");
+     btnThree.classList.remove("btn-click");     
+    }
 function twoSlide(){
-    banner.style.transform = "translate(-100vw)"
+    banner.style.transform = "translateX(-33.33%)";
+    btnOne.classList.remove("btn-click");
+    btnTwo.classList.add("btn-click");
+    btnThree.classList.remove("btn-click");  
 }
 
 function threeSlide(){
-    banner.style.transform = "translate(-200vw)"
+    banner.style.transform = "translateX(-66.66%)";
+    btnOne.classList.remove("btn-click");
+    btnTwo.classList.remove("btn-click");
+    btnThree.classList.add("btn-click");  
 }
-
 
 btnOne.addEventListener("click", oneSlide);
 btnTwo.addEventListener("click", twoSlide);
 btnThree.addEventListener("click", threeSlide);
+
+
+
